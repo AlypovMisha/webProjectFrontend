89080f21430a3ef5ecc03780b262a54b18fb7e54
Kiborg010 <evgeni.kibardin@yandex.ru>
2025-12-16 23:19:59 +0500

    update ProductCard as type

--- src/widgets/ProductCard/ui/ProductCard.vue
+++ src/widgets/ProductCard/ui/ProductCard.vue
@@ -7,20 +7,14 @@ defineProps<{ product: Product }>()
 
 <template>
   <q-card class="my-card" flat>
-
     <img :src="product.imageUrl" alt="Product image" />
 
     <q-card-section>
-      <!-- <RouterLink :to="`/product/${product.id}`"> -->
       <RouterLink :to="`/product/`">
         <div class="text-h6">{{ product.title }}</div>
       </RouterLink>
       <div class="text-h6" style="font-weight: bold">
         <span style="font-size: 20px" class="operText">{{ product.price }}₽</span>
-      </div>
-      <div>
-        <span class="prevPrice">{{ product.prevPrice }}₽</span>
-        <span class="bargainText">-{{ product.discount }}%</span>
       </div>
     </q-card-section>
 
@@ -48,15 +42,5 @@ defineProps<{ product: Product }>()
   background-clip: text;
   background-image: linear-gradient(225deg, rgb(241, 17, 126) 0%, rgb(241, 17, 126) 100%);
   color: transparent;
-}
-.prevPrice {
-  color: rgba(153, 163, 174);
-  text-decoration: line-through;
-  font-size: 14px;
-}
-.bargainText {
-  color: rgba(241, 17, 126, 1);
-  font-size: 14px;
-  margin-left: 4px;
 }
 </style>
