74144369eb6f38b67519cc49bb3d68019ba5754a
Kiborg010 <evgeni.kibardin@yandex.ru>
2025-12-18 00:18:31 +0500

    fix(DemoProductsGrid): use Product array prop

--- src/widgets/DemoProductsGrid/ui/DemoProductsGrid.vue
+++ src/widgets/DemoProductsGrid/ui/DemoProductsGrid.vue
@@ -1,37 +1,37 @@
-<script>
+<script lang="ts">
 import ProductCard from "src/widgets/ProductCard/ui/ProductCard.vue";
+import type { Product } from 'entities/Product/models/Product'
+import { defineComponent } from 'vue'
 
-export default {
+export default defineComponent({
   name: "DemoProductsGrid",
   components: { ProductCard },
 
-  data() {
-    return {
-      demoProducts: [
-        {
-          id: 1,
-          name: 'Товар с алиэкспресс',
-          price: 300,
-          order_count: 10,
-          photos: '["https://cdn.quasar.dev/img/mountains.jpg"]',
-          reviewsCount: 228
-        },
-        // добавь остальные если надо
-      ]
+  props: {
+    products: {
+      type: Array as () => Product[],
+      default: () => [] // если проп не передан, будет пустой массив
     }
   }
-}
+})
 </script>
 
 <template>
-  <div class="row">
-    <div 
-      class="col-lg-3 col-md-4 col-sm-6 col-12"
-      :style="prodIndex % 4 === 0 ? 'padding: 15px 15px 15px 0;' : 'padding: 15px 15px;'"
-      v-for="(prod, prodIndex) in demoProducts"
-      :key="prod.id"
-    >
-      <ProductCard :product="prod"/>
+  <div>
+    <!-- если массив пустой, показываем сообщение -->
+    <div v-if="products.length === 0" class="text-center">
+      Товары не найдены
+    </div>
+
+    <div class="row" v-else>
+      <div 
+        class="col-lg-3 col-md-4 col-sm-6 col-12"
+        v-for="(product, index) in products"
+        :key="product.product_id"
+        :style="index % 4 === 0 ? 'padding: 15px 15px 15px 0;' : 'padding: 15px 15px;'"
+      >
+        <ProductCard :product="product"/>
+      </div>
     </div>
   </div>
 </template>
